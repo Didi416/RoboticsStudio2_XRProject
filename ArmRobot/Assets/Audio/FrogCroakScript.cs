baa7496b220197a741e62ff44fce8606c138108e
Angelyn Co <145760222+PotatoPuffs@users.noreply.github.com>
2026-04-08 17:42:45 +1000

    basic

--- ArmRobot/Assets/Audio/FrogCroakScript.cs
+++ ArmRobot/Assets/Audio/FrogCroakScript.cs
@@ -1,19 +1,76 @@
+
+// using UnityEngine;
+// using System.Collections;
+
+// public class FrogCroakScript : MonoBehaviour
+// {
+//     public AudioSource audioSource;
+//     public int maxPlays = 3;
+//     public float timeBetweenCroaks = 0f;
+//     private bool hasPlayed = false;
+
+//     public void PlaySound()
+//     {
+//         if (!hasPlayed && audioSource != null)
+//         {
+//             hasPlayed = true;
+//             StartCoroutine(PlayMultipleTimes());
+//         }
+//     }
+
+//     private IEnumerator PlayMultipleTimes()
+//     {
+//         for (int i = 0; i < maxPlays; i++)
+//         {
+//             audioSource.Play();
+//             yield return new WaitForSeconds(audioSource.clip.length + timeBetweenCroaks);
+//         }
+//     }
+
+//     public void ResetSound()
+//     {
+//         hasPlayed = false;
+//         audioSource.Stop();
+//     }
+// }
+
 using UnityEngine;
+using System.Collections;
 
 public class FrogCroakScript : MonoBehaviour
 {
     public AudioSource audioSource;
-    public int maxPlays = 3;
+    public float timeBetweenCroaks = 0f;
 
-    private int playCount = 0;
+    private bool hasPlayed = false;
+    private int maxPlays;
 
     public void PlaySound()
     {
-        if (playCount < maxPlays && audioSource != null)
+        if (!hasPlayed && audioSource != null)
+        {
+            hasPlayed = true;
+
+            // Random number between 1 and 9 (inclusive)
+            maxPlays = Random.Range(1, 10);
+
+            StartCoroutine(PlayMultipleTimes());
+        }
+    }
+
+    private IEnumerator PlayMultipleTimes()
+    {
+        for (int i = 0; i < maxPlays; i++)
         {
             audioSource.Play();
-            playCount++;
+            yield return new WaitForSeconds(audioSource.clip.length + timeBetweenCroaks);
         }
+    }
+
+    public void ResetSound()
+    {
+        hasPlayed = false;
+        audioSource.Stop();
     }
 }
 
